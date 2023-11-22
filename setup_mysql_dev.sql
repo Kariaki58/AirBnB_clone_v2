1a16ccf0637aa176cfc19b180b57125570121013
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-22 23:52:04 +0100

    setup mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,5 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
-
-
-storage_type = os.environ.get("HBNB_TYPE_STORAGE")
-
-
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        state = relationship("City", cascade="all, delete", backref="state")
-    else:
-        name = ""
+-- sqldb create a database with a new db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
