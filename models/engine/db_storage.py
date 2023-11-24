ff2559a22c13c1b1a97b9a844c37f93b6f54a6a2
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-24 16:39:39 +0100

    db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,7 +66,7 @@ class DBStorage:
         from models.city import City, Base
         from models.place import Place, Base
         from models.state import State, Base
-        from models.amenity import Amenity, Base
+        from models.amenity import Amenity
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
