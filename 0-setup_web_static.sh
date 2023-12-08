5cc26a66f2ec7473b75b4c27ba27cf741b061f83
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-08 07:21:29 -0800

    checking if nginx exits

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-if ! command -v nginx &> /dev/null; then
+if test -d "/etc/nginx"; then
     sudo apt-get -y update
     sudo apt-get -y install nginx
 fi
