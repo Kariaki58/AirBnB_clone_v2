3b3942d73ec456515cbcf59bfbfe5db387cabc19
Kariaki58 <kariakichernocherno@gmail.com>
2023-12-10 00:42:21 -0800

    fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,46 @@
 #!/usr/bin/python3
-"""importing... modules"""
-from fabric.api import run, put, env
+"""importing modules"""
+from fabric.api import env, put, run
 from os.path import exists
 
 
 env.hosts = ["100.25.179.45", "100.25.30.209"]
 
+
 def do_deploy(archive_path):
-    """do deploy"""
-    check_file = exists(archive_path)
-    if not check_file:
+    """deploy function"""
+    if not exists(archive_path):
         return False
-    put(archive_path, "/tmp/")
+    file_name = archive_path.split("/")[-1]
+    name = file_name.split(".")[0]
 
-    archive_filename = archive_path.split("/")[-1]
-    release_path = "/data/web_static/releases/{}".format(
-            archive_filename[:-4]
-            )
-    print("here")
-    run("mkdir -p {}".format(release_path))
-    run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_path))
-
-    run("rm /tmp/{}".format(archive_filename))
-
-    run("mv {}/web_static/* {}".format(release_path, release_path))
-
-    run("rm -rf {}/web_static".format(release_path))
-
-    run("rm -rf /data/web_static/current")
-
-    run("ln -s {} /data/web_static/current".format(release_path))
-
-    print("New version deployed!")
+    if put(archive_path, "/tmp/{}".format(file_name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".format(
+        name
+        )).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".format(
+        name
+        )).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+        file_name, name
+        )).failed is True:
+        return False
+    if run("rm /tmp/{}".format(
+        file_name
+        )).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
     return True
-
-
-if __name__ == "__main__":
-    """entry point"""
-    archive_path = "versions/web_static_20231209082910.tgz"
-
-    private_key_path = "~/.ssh/id_rsa"
-    username = "ubuntu"
-
-    result = do_deploy(archive_path)
-
-    if result:
-        print("Deployment successful!")
-    else:
-        print("Deployment failed!") 
-
