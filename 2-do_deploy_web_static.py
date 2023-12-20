8ef885aaad8a9d7cec58733b8ac677b8a8334671
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-20 05:12:30 -0800

    modifying solution

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['54.205.99.166', '100.26.122.165']
+env.hosts = ['54.197.122.71', '34.207.61.76']
 
 
 def do_deploy(archive_path):
@@ -25,26 +25,26 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
+    if run("sudo rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
+    if run("sudo mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+    if run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("sudo rm /tmp/{}".format(file)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
+    if run("sudo mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
+    if run("sudo rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
