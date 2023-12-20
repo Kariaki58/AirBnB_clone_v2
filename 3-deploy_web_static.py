7feaf29640112e83beb3c42957a29220108ce875
Kariaki58 <kariakichernocherno@gmail.com>
2023-12-20 13:09:56 +0100

    full deployment with fabric

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 """import modules"""
 import os.path
-from fabric.api import env, put, run
+from datetime import datetime
+from fabric.api import env, local, put, run
 
-env.hosts = ['54.197.122.71', '34.207.61.76']
+
+env.hosts = ['54.205.99.166', '100.26.122.165']
+
+
+def do_pack():
+    """pack a file for deployment"""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+            )
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
 
 
 def do_deploy(archive_path):
-    """Airbnb clone deployment"""
+    """deployment"""
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
@@ -38,3 +54,11 @@ def do_deploy(archive_path):
            format(name)).failed is True:
         return False
     return True
+
+
+def deploy():
+    """Create and deploy an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
