3610b2d69b7241461423ba7f10fbc41e4b30c593
Kariaki58 <kariakichernocherno@gmail.com>
2023-12-20 14:19:41 +0100

    do web deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-# A script that transfers an archive to a web server.
-
+"""importing modules"""
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run
+
 
 env.hosts = ['54.197.122.71', '34.207.61.76']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
+    """web static deployment"""
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
