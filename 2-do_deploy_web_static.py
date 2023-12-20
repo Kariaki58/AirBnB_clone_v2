8fa00eda9cc166cba9b7c3a604c36636e62188de
Kariaki58 <kariakichernocherno@gmail.com>
2023-12-20 13:58:08 +0100

    cloned

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,50 @@
 #!/usr/bin/python3
-"""Fabric script to deploy web_static"""
+# A script that transfers an archive to a web server.
 
-from fabric.api import env, put, run
-from os.path import exists
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-env.hosts = ['54.197.122.71', '34.207.61.76']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ['54.205.99.166', '100.26.122.165']
 
 
 def do_deploy(archive_path):
-    """Deploy web_static to web servers"""
-    if not exists(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        # Upload the archive to /tmp/ directory on the web server
-        put(archive_path, '/tmp/')
-
-        # Extract the archive to /data/web_static/releases/
-        filename = archive_path.split("/")[-1]
-        folder_name = "/data/web_static/releases/{}".format(
-            filename.split(".")[0])
-        run("sudo mkdir -p {}".format(folder_name))
-        run("sudo tar -xzf /tmp/{} -C {}".format(filename, folder_name))
-
-        # Delete the archive from the web server
-        run("sudo rm /tmp/{}".format(filename))
-
-        # Move contents to the proper location
-        run("sudo mv {}/web_static/* {}".format(folder_name, folder_name))
-
-        # Remove the web_static directory
-        run("sudo rm -rf {}/web_static".format(folder_name))
-
-        # Remove the current symbolic link
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
-        run("sudo ln -s {} /data/web_static/current".format(folder_name))
-
-        print("New version deployed!")
-        return True
-
-    except Exception as e:
-        print("Deployment failed:", str(e))
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
