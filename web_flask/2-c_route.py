d086457b5d16e10c3c3bf0545cee2a8c3665ae76
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-25 21:06:39 +0100

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display():
+    """return a string"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """return HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """display c and the value of the text"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
