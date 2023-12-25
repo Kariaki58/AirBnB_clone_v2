a6b8462670f0bbd3aef6d136ba74c14a9ae3bf87
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-25 21:30:36 +0100

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display():
+    """return a string"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """return HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """display c and the value of the text"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """python is cool"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """number route"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
