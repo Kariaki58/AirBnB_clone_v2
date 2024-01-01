62780ea9869d6ce2256c06140087920e012227bd
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-01 16:33:52 +0100

    wahala

--- models/base_model.py
+++ models/base_model.py
@@ -17,10 +17,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             time_format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == "__class__":
