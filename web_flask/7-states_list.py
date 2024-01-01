b8ab7ed36a7de821626b5d139eff11c5420fad02
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-01 14:07:04 +0100

    list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+
+app = Flask(__name__)
+
+states = storage.all(State)
+
+@app.teardown_appcontext
+def remove_session(session):
+    """close session after each request"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    dictionary = {}
+    listofdictionary = []
+    for data in states.values():
+        dictionary['name'] = data.name
+        dictionary['id'] = data.id
+        listofdictionary.append(dictionary)
+    sorted_data = sorted(listofdictionary, key=lambda x: x['name'])
+    return render_template('7-states_list.html', sorted_data=sorted_data)
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)
