18968e687e0c64f1fafe2ff0196f2daa48bca858
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-01 17:16:10 +0100

    fixed pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,10 +68,10 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
-    
+
     def remove(self):
         """remove method"""
         self.__session.close()
-    
+
     def close(self):
         self.__session.close()
