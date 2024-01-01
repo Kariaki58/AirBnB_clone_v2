b66ea3189034f85aa0f079d681f5fdf138c649e4
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-01 17:12:38 +0100

    updated code

--- models/base_model.py
+++ models/base_model.py
@@ -17,11 +17,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
             time_format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key == "__class__":
