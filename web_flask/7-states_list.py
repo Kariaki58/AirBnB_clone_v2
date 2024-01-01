ad31ad94eb8b054df969b8f2c304c32163aa1a0d
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-01 14:12:51 +0100

    list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 states = storage.all(State)
 
+
 @app.teardown_appcontext
 def remove_session(session):
     """close session after each request"""
@@ -28,5 +29,5 @@ def states_list():
     return render_template('7-states_list.html', sorted_data=sorted_data)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
